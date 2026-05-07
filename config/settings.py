c96647d09b2d7e2d6486a58226c5f30fa2c867fc
Andy <steeven.8.6.ap@gmail.com>
2026-05-07 17:45:36 -0500

    activando debug para ver error

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f5r0vm1bjvs10$wjcld7nm-bg@-@8&00ye(7k%j%k@j446=c2e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
