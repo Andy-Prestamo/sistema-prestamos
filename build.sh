f9c01b5eb36dee2d2b45b5003873b2d413ed4f2c
Andy <steeven.8.6.ap@gmail.com>
2026-05-08 00:26:16 -0500

    Configuracion final sin scripts de usuario

--- build.sh
+++ build.sh
@@ -2,10 +2,11 @@
 # exit on error
 set -o errexit
 
+# Instalar las librerías necesarias
 pip install -r requirements.txt
 
+# Recopilar archivos estáticos (diseños, CSS)
 python manage.py collectstatic --no-input
-python manage.py migrate
 
-# Esta línea crea el usuario automáticamente usando el otro archivo que creaste
-python crear_usuario.py
+# Aplicar cualquier cambio en la estructura de la base de datos
+python manage.py migrate
