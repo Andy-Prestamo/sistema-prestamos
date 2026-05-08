d9dd51221cc0d5c762f0b86e7e0141d79f1b2b87
Andy <steeven.8.6.ap@gmail.com>
2026-05-08 00:44:40 -0500

    Corrigiendo rutas de login y logout

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views # Importante
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.name),
     path('', include('prestamos.urls')),
-    path('accounts/', include('django.contrib.auth.urls')), # Agrega esta línea
+    
+    # Esta línea es la que falta para que reconozca /login/
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
