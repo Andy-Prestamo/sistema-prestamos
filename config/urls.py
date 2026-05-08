a4e96e9f499501f1d26719ba1f155f3cd31ac821
Andy <steeven.8.6.ap@gmail.com>
2026-05-08 00:48:45 -0500

    Arreglando urls para desbloquear render

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views as auth_views # Importante
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('admin/', admin.site.name),
+    path('admin/', admin.site.urls), # Verifica que diga .urls aquí
     path('', include('prestamos.urls')),
     
-    # Esta línea es la que falta para que reconozca /login/
-    path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # Rutas de autenticación
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
